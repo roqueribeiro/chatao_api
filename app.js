256293f68e427f60d633c9394de94f6f56c722a3
Roque Ribeiro <roque.ribeiro@hotmail.com.br>
2020-02-03 10:54:00 -0300

    fix

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ const express = require('express');
 const application = express();
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 const dotenv = require('dotenv');
 const server = require('http').createServer([], application);
 dotenv.config();
 
+app.use(cors());
 app.use(bodyParser.json());
-api.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', '*');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
+
+app.get('/', function(req, res) {
+    res.send('Chatao API Works');
 });
 
 global.io = require('socket.io').listen(server);
@@ -21,6 +20,6 @@ require('./api/sockets/genericMethods')(io);
 application.set('port', process.env.PORT);
 application.use(app);
 
-server.listen(application.get('port'), function () {
+server.listen(application.get('port'), () => {
     console.log('Express app listening at http://%s:%s', server.address().address, server.address().port);
 });
