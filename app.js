ee70218edc49518f3354b9b85f5f21f6951a2c85
Roque Ribeiro da Silva <roque.ribeiro@hotmail.com.br>
2020-03-06 16:14:51 -0300

    Enable Cors

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require('express');
 const application = express();
 const bodyParser = require('body-parser');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 const dotenv = require('dotenv');
 const server = require('http').createServer([], application);
 dotenv.config();
 
-app.use(cors());
+// app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
